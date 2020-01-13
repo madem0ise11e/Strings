f5d565c25ea52bc3620021c45c94f30b5316c2eb
Tetiana Kizan <t.kizan@studenti.unina.it>
2020-01-13 16:29:45 +0100

    Adjust

--- CountVowelsConsonants.c
+++ CountVowelsConsonants.c
@@ -12,7 +12,7 @@ int main (){
     if(line[i] == 'a' || line[i] == 'e' || line[i] == 'i' ||
        line[i] == 'o' || line[i] == 'u' || line[i] == 'A' ||
        line[i] == 'E' || line[i] == 'I' || line[i] == 'O' ||
-       line[i] == 'U' ||){
+       line[i] == 'U'){
          ++vowels;
        } else if ((line[i] >= 'a' && line[i] <= 'z') || (line[i] >= 'A' && line[i] <= 'Z')){
          ++consonant;
