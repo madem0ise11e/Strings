3faafa4926758e6a8ffa63d6ead3d84b5e4449e2
Tetiana Kizan <t.kizan@studenti.unina.it>
2020-02-14 10:31:47 +0100

    create es5a.c

---
+++ es5a.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#define MAXSTR 80
+
+int main () {
+  char a[MAXSTR], b[11] = {'\0'};
+  int i;
+
+  printf("Stringa? ");
+  gets(a);
+  strncpy(b,a,10)
+  if (strchr(b, 'A') != NULL)
+    printf("Trovato\n");
+  else
+    printf("Non trovato\n");
+  return 0;
+}
